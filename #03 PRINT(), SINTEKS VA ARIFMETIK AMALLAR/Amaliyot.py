7090a16baa9251e66d8fa40ecdc21344a8d8c17f
Shohruh Ravshanov <Shohruhh077@gmail.com>
2022-03-09 12:20:57 +0300

    3-Dars qo`shildi

---
+++ #03 PRINT(), SINTEKS VA ARIFMETIK AMALLAR/Amaliyot.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 12:16:42 2022
+
+@author: Shohruhh077
+"""
+
+# Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+print('"Nexia", "Tico", \'Damas\' ko`rganlar qilar havas')
+
+# Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+# 5 ning 4-darajasini toping
+print("5 ning 4-darajasi ", 5**4, " ga teng")
+
+# 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4 ga bo`lganda ", 22%4, " qoldiq qoladi")
+
+# Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Tomonlari 125 ga teng bo`lgan kvadratning yuzi ", 125**2, " ga, perimetri esa ", 125*4, " ga teng")
+
+# Diametri 12 ga teng bo'lgan doiraning yuzini toping  (pi=3.14 deb oling)
+print("Diametri 12 ga teng bo`lgan doiraning yuzi ", 3.14*(12/2)**2, " ga teng")
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini ", (6**2+7**2)**(1/2), " ga teng")
