75cf513b5881abdb454c6274e217cfc1fe9dc56a
Shohruh Ravshanov <Shohruhh077@gmail.com>
2022-03-09 12:09:41 +0300

    2-Dars qo`shildi

---
+++ #02 HELLO WORLD!/Amaliyot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  9 12:05:10 2022
+
+@author: Shohruhh077
+"""
+"""
+Spyder konsolida va matn muharririda quyidagi kodlarni yozib,
+bajarib ko'ring. Siz kutgan natijalar chiqdimi?
+"""
+
+print('Assalom alaykum')
+# print(Hayrli tong!) #{Xato chiqaradi. Chunki Qo`shtirnoq yoki birtirnoq qo`llanilmagan}
+print(2+4*2)
+print(19/5)
+print(2**4)
