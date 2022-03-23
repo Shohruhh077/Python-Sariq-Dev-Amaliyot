dbadeda037f5796462a72e6bb3b110e784842fe9
Shohruh Ravshanov <Shohruhh077@gmail.com>
2022-03-23 19:15:56 +0300

    Yangi dars qo`shildi

---
+++ #17 WHILE TSIKLI/Amaliyot.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 19:03:38 2022
+
+@author: Shohruhh077
+"""
+
+"""
+Foydalanuvchidan yaxshi ko'rgan kitoblarini kiritishni so'rang.
+Foydalanuvchi stop so'zini yozishi bilan dasturni to'xtating
+"""
+# savol = "Yoqtirgan kitobingizni kiriting"
+# savol += "(Barcha kitoblaringizni kiritib bo`lgach \"exit\" deb yozing):\n"
+
+# while True:
+#     kitob = input(savol)
+#     if kitob == 'exit':
+#         break
+# print("Dastur tugatildi")
+
+
+
